c4e9c8278a0921731e963acf45559058bf2d83d0
Denys Kharkov <kharkov.denys@lll.kpi.ua>
2023-04-29 12:28:39 +0200

    Updated CORS

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
-from flask import Flask
+from flask import Flask, Response, request
 from flask_cors import CORS
 from api.routes.playlist_bp import playlist_bp
 from api.routes.music_bp import music_bp
 
 myapi = Flask(__name__)
 CORS(myapi)
+
+
+@myapi.before_request
+def basic_authentication():
+    if request.method.lower() == 'options':
+        return Response()
+
 
 myapi.register_blueprint(music_bp)
 myapi.register_blueprint(playlist_bp)
